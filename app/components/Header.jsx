b5f34fdf8685f9082e64a2973086a7b00efc6416
mmchokor <mohameddd701@gmail.com>
2023-08-28 12:45:41 +0300

    Email Signup & Verification Code

---
+++ app/components/Header.jsx
@@ -0,0 +1,52 @@
+import { auth, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const Header = () => {
+   const { userId } = auth()
+   console.log(userId)
+
+   return (
+      <>
+         <nav className='bg-blue-700 py-4 px-6 flex items-center justify-between mb-5'>
+            <div className='flex items-center'>
+               <Link href='/'>
+                  <div className='text-lg uppercase text-white font-bold'>
+                     Clerk App
+                  </div>
+               </Link>
+            </div>
+            <div className='text-white flex items-center'>
+               {!userId && (
+                  <>
+                     <Link
+                        href='sign-in'
+                        className='text-gray-300 hover:text-white mr-4'
+                     >
+                        Sign In
+                     </Link>
+                     <Link
+                        href='sign-up'
+                        className='text-gray-300 hover:text-white mr-4'
+                     >
+                        Sign Up
+                     </Link>
+                  </>
+               )}
+               {userId && (
+                  <Link
+                     href='/profile'
+                     className='text-gray-300 hover:text-white mr-4'
+                  >
+                     Profile
+                  </Link>
+               )}
+               <div className='ml-auto'>
+                  <UserButton afterSignOutUrl='/' />
+               </div>
+            </div>
+         </nav>
+      </>
+   )
+}
+
+export default Header
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DashboardPage = () => {
+   return (
+      <>
+         <h1 className='text-2xl font-bold mb-5'>
+            <p className='mb-5'>Welcome to the Dashboard</p>
+         </h1>
+      </>
+   )
+}
+
+export default DashboardPage
